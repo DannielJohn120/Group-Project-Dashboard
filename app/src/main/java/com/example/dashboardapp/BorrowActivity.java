f9c236dc6220522b1e7e1be4f4a279094bc88ca3
Kim052523 <etomotorgo.a12344976@umak.edu.ph>
2025-05-10 13:59:29 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/dashboardapp/BorrowActivity.java
@@ -0,0 +1,13 @@
+package com.example.dashboardapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BorrowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_borrow); // XML layout you'll create next
+    }
+}
